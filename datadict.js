47558eee58bec0a959f88193ec6b1c474aa1d9bb
danmunz <daniel.a.munz@gmail.com>
2013-05-13 22:10:25 -0400

    First *real* commit
    Commit…well, everything, more or less.

---
+++ datadict.js
@@ -0,0 +1,174 @@
+var hudagencies = {
+    "features": [
+	   {
+
+      "agcid":"81961",
+
+      "adr1":"3220 Pennsylvania Avenue SE, Suite 4",
+
+      "adr2":" ",
+
+      "city":"WASHINGTON",
+
+      "email":"jbrown@uls-dc.com",
+
+      "fax":"202-547-2083",
+
+      "nme":"UNIVERSITY LEGAL SERVICES",
+
+      "phone1":"202-547-4747",
+
+      "statecd":"DC",
+
+      "weburl":"http://n/a",
+
+      "zipcd":"20020-3712",
+
+      "agc_ADDR_LONGITUDE":"-76.960016",
+
+      "agc_ADDR_LATITUDE":"38.86913",
+
+      "languages":"ENG",
+
+      "services":"DFC,FBC,HIC,HMC,RHC"
+
+   },
+
+   {
+
+      "agcid":"80219",
+
+      "adr1":"1173 W. Main Street",
+
+      "adr2":" ",
+
+      "city":"ABINGDON",
+
+      "email":"jgross@peopleinc.net",
+
+      "fax":"276-628-2931",
+
+      "nme":"PEOPLE INCORPORATED OF SOUTHWEST VIRGINIA",
+
+      "phone1":"276-623-9000-2201",
+
+      "statecd":"VA",
+
+      "weburl":"http://www.peopleinc.net",
+
+      "zipcd":"24210-4703",
+
+      "agc_ADDR_LONGITUDE":"-82.013917",
+
+      "agc_ADDR_LATITUDE":"36.697010",
+
+      "languages":"SPA",
+
+      "services":"FHW,HIC,NDW,PPW,RHC"
+
+   },
+   
+   {
+
+      "agcid":"80239",
+
+      "adr1":"255 Orange St",
+
+      "adr2":" ",
+
+      "city":"ALBANY",
+
+      "email":"N/A",
+
+      "fax":"N/A",
+
+      "nme":"AFFORDABLE HOUSING PARTNERSHIP",
+
+      "phone1":"518-434-1730",
+
+      "statecd":"NY",
+
+      "weburl":"http://www.ahphome.org",
+
+      "zipcd":"12210-2400",
+
+      "agc_ADDR_LONGITUDE":"-73.757325",
+
+      "agc_ADDR_LATITUDE":"42.657815",
+
+      "languages":"ASL,SPA",
+
+      "services":"FHW,PPW,RHC,FBC,HIC"
+
+   },
+   
+      {
+
+      "agcid":"83519",
+
+      "adr1":"841 E 2ND",
+
+      "adr2":" ",
+
+      "city":"CARSON CITY",
+
+      "email":"ccanv@aol.com",
+
+      "fax":"N/A",
+
+      "nme":"CONSUMER CREDIT COUNSELING SERVICE OF SOUTHERN NEVADA",
+
+      "phone1":"800-451-4505",
+
+      "statecd":"NV",
+
+      "weburl":"http://www.cccsnevada.org",
+
+      "zipcd":"12210-2400",
+
+      "agc_ADDR_LONGITUDE":"-119.760293",
+
+      "agc_ADDR_LATITUDE":"39.162960",
+
+      "languages":"ENG,SPA",
+
+      "services":"FHW,PPW"
+
+   },
+   
+     {
+
+      "agcid":"83019",
+
+      "adr1":"115 W. Chicago Ave",
+
+      "adr2":" ",
+
+      "city":"CHICAGO",
+
+      "email":"dfeidt@accessliving.org",
+
+      "fax":"312-640-2101",
+
+      "nme":"ACCESS LIVING OF METROPOLITAN CHICAGO",
+
+      "phone1":"312-640-2100-2197",
+
+      "statecd":"IL",
+
+      "weburl":"http://N/A",
+
+      "zipcd":"60654-3209",
+
+      "agc_ADDR_LONGITUDE":"-87.632235",
+
+      "agc_ADDR_LATITUDE":"41.896619",
+
+      "languages":"ASL,ENG,SPA",
+
+      "services":"HIC,NDW,PPW"
+
+   }
+	]
+	
+	}
