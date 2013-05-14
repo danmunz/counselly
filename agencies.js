9252ab6c2455cfb30420466cb9724d410bd3cfb9
danmunz <daniel.a.munz@gmail.com>
2013-05-14 11:19:40 -0400

    Added two new sample agencies
    Close to Alexandria, for demo purposes

--- agencies.js
+++ agencies.js
@@ -168,6 +168,73 @@ var hudagencies = {
 
       "services":"HIC,NDW,PPW"
 
+   },
+   
+        {
+
+      "agcid":"83019",
+
+      "adr1":"801 N Pitt St",
+
+      "adr2":"Apt 117",
+
+      "city":"ALEXANDRIA",
+
+      "email":"counselinginfo@moneymanagement.org",
+
+      "fax":"866-921-5129",
+
+      "nme":"MONEY MANAGEMENT INTERNATIONAL ALEXANDRIA VA",
+
+      "phone1":"866-232-9080",
+
+      "statecd":"VA",
+
+      "weburl":"http://www.moneymanagement.org",
+
+      "zipcd":"22314-1783",
+
+      "agc_ADDR_LONGITUDE":"-77.042357",
+
+      "agc_ADDR_LATITUDE":"38.813227",
+
+      "languages":"ENG,SPA",
+
+      "services":"DFC,HIC,RHC"
+
+   },
+          {
+
+      "agcid":"83019",
+
+      "adr1":"2300 9th St S",
+
+      "adr2":"Ste 300B",
+
+      "city":"ARLINGTON",
+
+      "email":"N/A",
+
+      "fax":"312-640-2101",
+
+      "nme":"AHOME",
+
+      "phone1":"703-527-3854",
+
+      "statecd":"VA",
+
+      "weburl":"http://www.ahomeinc.org",
+
+      "zipcd":"22204-2345",
+
+      "agc_ADDR_LONGITUDE":"-77.082990",
+
+      "agc_ADDR_LATITUDE":"38.864607",
+
+      "languages":"ENG",
+
+      "services":"HIC,NDW,RHC"
+
    }
 	]
 	
